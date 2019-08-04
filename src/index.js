f39876621ced7e14387caaf7d521c69f88b0e265
Kory Newton <korybnewton@gmail.com>
2019-08-04 10:46:41 -0700

    css/style loaders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { run } from './app/app';
 import { AlertService } from './app/alert.service';
 import { ComponentService } from './app/component.service';
+import './main.css';
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
