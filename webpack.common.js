a3bb4af3012266f8872f8359d36619bb0d29ff25
Kory Newton <korybnewton@gmail.com>
2019-08-05 16:40:47 -0700

    html-loader-file, file-loader, clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,20 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(svg|png|gif|jpg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs'
+          }
+        }
       }
     ]
   }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = merge(common, {
   mode: 'production',
   //   devtool: 'none',
@@ -8,5 +10,6 @@ module.exports = merge(common, {
   output: {
     filename: 'main.[contentHash].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [new CleanWebpackPlugin()]
 });
