4126e85059c17289da0eea874866687552cd8038
Kory Newton <korybnewton@gmail.com>
2019-08-05 17:52:37 -0700

    minified

--- webpack.common.js
+++ webpack.common.js
@@ -2,22 +2,12 @@ const path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
   entry: {
     main: './src/index.js',
     vendor: './src/vendor.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html'
-    })
-  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
       {
         test: /\.html$/,
         use: ['html-loader']
